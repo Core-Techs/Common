8ceed4e7cca62b9fe7b2c7c47e7267efc314a961
Ronnie Overby <roverby@coretechs.net>
2014-07-24 18:53:33 -0400

    some reflection

---
+++ CoreTechs.Common/ConnectionScope.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreTechs.Common
+{
+    /// <summary>
+    /// Opens the connection on construction if it's not already open.
+    /// On disposal, the connection will be closed if it was closed before construction.
+    /// </summary>
+    public class ConnectionScope : IDisposable
+    {
+        private readonly DbConnection _connection;
+        private readonly ConnectionState _initState;
+
+        public ConnectionScope(DbConnection connection)
+        {
+            if (connection == null) throw new ArgumentNullException("connection");
+
+            _connection = connection;
+            _initState = connection.State;
+
+        }
+
+        public void Open()
+        {
+            if (_connection.State != ConnectionState.Open)
+                _connection.Open();
+        }
+
+        public async Task OpenAsync()
+        {
+            if (_connection.State != ConnectionState.Open)
+                await _connection.OpenAsync();
+        }
+
+        public void Dispose()
+        {
+            if (_initState == ConnectionState.Closed && _connection.State != ConnectionState.Closed)
+                _connection.Close();
+        }
+    }
+}
---
+++ CoreTechs.Common/Reflection/PropertyExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CoreTechs.Common.Reflection
+{
+    public static class PropertyExtensions
+    {
+        private static readonly ConcurrentDictionary<object, Dictionary<string, PropertyInfo>> PropertyCache =
+            new ConcurrentDictionary<object, Dictionary<string, PropertyInfo>>();
+
+        private static readonly ConcurrentDictionary<object, Func<object, object>> GetterCache =
+            new ConcurrentDictionary<object, Func<object, object>>();
+
+        public static PropertyInfo[] GetAllProperties<T>()
+        {
+            return GetAllProperties(typeof (T));
+        }
+
+        public static PropertyInfo[] GetAllProperties(Type type)
+        {
+            var props = CacheProperties(type).Values;
+            return props.ToArray();
+        }
+
+        public static PropertyInfo[] GetAllProperties(this object source)
+        {
+            return GetAllProperties(source.GetType());
+        }
+
+        public static PropertyInfo FindProperty(this object source, string name)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var props = CacheProperties(source.GetType());
+            return props.ContainsKey(name) ? props[name] : null;
+        }
+
+        private static Dictionary<string, PropertyInfo> CacheProperties(Type type)
+        {
+            return PropertyCache.GetOrAdd(type, k => type.GetRuntimeProperties().ToDictionary(p => p.Name, p => p));
+            
+        }
+
+        public static object GetPropertyValue(this object source, string name)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (name.IsNullOrWhiteSpace()) throw new ArgumentNullException("name");
+
+            var key = new { Type = source.GetType(), name };
+
+            var getter = GetterCache.GetOrAdd(key, k =>
+            {
+                var prop = key.Type.FindProperty(name);
+
+                if (prop == null)
+                {
+                    throw new ArgumentOutOfRangeException("name", "Property not found: " + name);
+                }
+
+                var instance = Expression.Parameter(typeof(object), "instance");
+
+                var instanceCast = (!prop.DeclaringType.IsValueType)
+                    ? Expression.TypeAs(instance, prop.DeclaringType)
+                    : Expression.Convert(instance, prop.DeclaringType);
+
+                var call = Expression.Call(instanceCast, prop.GetMethod);
+
+                var lambda = Expression.Lambda<Func<object, object>>(
+                    Expression.TypeAs(call, typeof (object)), instance);
+                
+                return lambda.Compile();
+
+            });
+
+            return getter(source);
+        }
+
+        public static TProp GetPropertyValue<TProp>(this object instance, string name)
+        {
+            return (TProp)instance.GetPropertyValue(name);
+        }
+
+        // todo setters
+    }
+}
