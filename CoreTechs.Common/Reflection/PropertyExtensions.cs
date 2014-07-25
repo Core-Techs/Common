7dc49b4b888b77636b6556fbc44d97e0e98efbe2
Ronnie Overby <ronnie@ronnieo.net>
2014-07-24 23:34:27 -0400

    mo prop reflection

--- CoreTechs.Common/Reflection/PropertyExtensions.cs
+++ CoreTechs.Common/Reflection/PropertyExtensions.cs
@@ -15,34 +15,38 @@ namespace CoreTechs.Common.Reflection
         private static readonly ConcurrentDictionary<object, Func<object, object>> GetterCache =
             new ConcurrentDictionary<object, Func<object, object>>();
 
+        private static readonly ConcurrentDictionary<object, Action<object, object>> SetterCache =
+            new ConcurrentDictionary<object, Action<object, object>>();
+
         public static PropertyInfo[] GetAllProperties<T>()
         {
-            return GetAllProperties(typeof (T));
+            return GetAllProperties(typeof(T));
         }
 
         public static PropertyInfo[] GetAllProperties(Type type)
         {
-            var props = CacheProperties(type).Values;
+            var props = GetCachedProperties(type).Values;
             return props.ToArray();
         }
 
         public static PropertyInfo[] GetAllProperties(this object source)
         {
-            return GetAllProperties(source.GetType());
+            var type = source.GetType();
+            return GetAllProperties(type);
         }
 
         public static PropertyInfo FindProperty(this object source, string name)
         {
             if (source == null) throw new ArgumentNullException("source");
 
-            var props = CacheProperties(source.GetType());
+            var type = source.GetType();
+            var props = GetCachedProperties(type);
             return props.ContainsKey(name) ? props[name] : null;
         }
 
-        private static Dictionary<string, PropertyInfo> CacheProperties(Type type)
+        private static Dictionary<string, PropertyInfo> GetCachedProperties(Type type)
         {
             return PropertyCache.GetOrAdd(type, k => type.GetRuntimeProperties().ToDictionary(p => p.Name, p => p));
-            
         }
 
         public static object GetPropertyValue(this object source, string name)
@@ -50,28 +54,29 @@ namespace CoreTechs.Common.Reflection
             if (source == null) throw new ArgumentNullException("source");
             if (name.IsNullOrWhiteSpace()) throw new ArgumentNullException("name");
 
+            var prop = source.FindProperty(name);
+
+            if (prop == null)
+            {
+                throw new ArgumentOutOfRangeException("name", "Property not found: " + name);
+            }
+
             var key = new { Type = source.GetType(), name };
 
             var getter = GetterCache.GetOrAdd(key, k =>
             {
-                var prop = key.Type.FindProperty(name);
-
-                if (prop == null)
-                {
-                    throw new ArgumentOutOfRangeException("name", "Property not found: " + name);
-                }
 
                 var instance = Expression.Parameter(typeof(object), "instance");
 
-                var instanceCast = (!prop.DeclaringType.IsValueType)
+                var instanceCast = !prop.DeclaringType.IsValueType
                     ? Expression.TypeAs(instance, prop.DeclaringType)
                     : Expression.Convert(instance, prop.DeclaringType);
 
-                var call = Expression.Call(instanceCast, prop.GetMethod);
+                var methodCallExpression = Expression.Call(instanceCast, prop.GetMethod);
 
                 var lambda = Expression.Lambda<Func<object, object>>(
-                    Expression.TypeAs(call, typeof (object)), instance);
-                
+                    Expression.TypeAs(methodCallExpression, typeof(object)), instance);
+
                 return lambda.Compile();
 
             });
@@ -84,6 +89,49 @@ namespace CoreTechs.Common.Reflection
             return (TProp)instance.GetPropertyValue(name);
         }
 
-        // todo setters
+
+        public static void SetPropertyValue(this object source, string name, object value)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (name.IsNullOrWhiteSpace()) throw new ArgumentNullException("name");
+            if (value == null) throw new ArgumentNullException("value");
+
+            var prop = source.FindProperty(name);
+
+            if (prop == null)
+            {
+                throw new ArgumentOutOfRangeException("name", "Property not found: " + name);
+            }
+
+            if (!prop.PropertyType.IsInstanceOfType(value))
+            {
+                throw new ArgumentException(
+                    string.Format("value of type {0} cannot be assigned to property of type {1}",
+                        value.GetType().Name, prop.PropertyType.Name));
+            }
+
+            var key = new { Type = source.GetType(), name };
+            var setter = SetterCache.GetOrAdd(key, k =>
+            {
+                var instance = Expression.Parameter(typeof(object), "instance");
+                var valueExp = Expression.Parameter(typeof(object), "value");
+
+                // value as T is slightly faster than (T)value, so if it's not a value type, use that
+                var instanceCast = (!prop.DeclaringType.IsValueType) ? Expression.TypeAs(instance, prop.DeclaringType) : Expression.Convert(instance, prop.DeclaringType);
+
+                var valueCast = (!prop.PropertyType.IsValueType) ? Expression.TypeAs(valueExp, prop.PropertyType) : Expression.Convert(valueExp, prop.PropertyType);
+
+                var lambda = Expression.Lambda<Action<object, object>>(
+                    Expression.Call(instanceCast, prop.GetSetMethod(), valueCast), new[] {instance, valueExp});
+
+                return lambda.Compile();
+
+            });
+
+            setter(source, value);
+        }
+
     }
+
+
 }
---
+++ Tests/Reflection/ReflectionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CoreTechs.Common.Reflection;
+using NUnit.Framework;
+
+namespace Tests.Reflection
+{
+    public class ReflectionTests
+    {
+        public string Name { get; set; }
+
+        [Test]
+        public void TestGetProperty()
+        {
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Name = i.ToString();
+                var name = this.GetPropertyValue<string>("Name");
+                Assert.AreEqual(Name, name);
+            }
+
+        }
+
+        [Test]
+        public void TestSetProperty()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                var name = i.ToString();
+                this.SetPropertyValue("Name", i.ToString());
+                Assert.AreEqual(name, Name);
+            }
+        }
+
+    }
+}
