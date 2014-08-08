69b77fc7a3657cba6af21640e0cb47a79edb1353
Ronnie Overby <roverby@coretechs.net>
2014-08-07 22:07:25 -0400

    minor clock changes

--- CoreTechs.Common/Clocks.cs
+++ CoreTechs.Common/Clocks.cs
@@ -11,20 +11,32 @@ namespace CoreTechs.Common
         DateTimeOffset Now { get; }
     }
 
+    /// <summary>
+    /// Clock that allows controlling the next value returned by the Now property.
+    /// </summary>
     public class TestClock : IClock
     {
-        private DateTimeOffset _now;
+        private DateTimeOffset _prevInstant;
+        private Func<DateTimeOffset, DateTimeOffset> _nextNowFunction;
 
         /// <summary>
         /// Provides the Now property's value. The previous value is passed to the function.
         /// </summary>
-        public Func<DateTimeOffset, DateTimeOffset> NextNowFunction { get; set; }
+        public Func<DateTimeOffset, DateTimeOffset> NextNowFunction
+        {
+            get { return _nextNowFunction; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                _nextNowFunction = value;
+            }
+        }
 
         public TestClock(DateTimeOffset initialInstant, Func<DateTimeOffset,DateTimeOffset> nextNowFunction)
         {
             if (nextNowFunction == null) throw new ArgumentNullException("nextNowFunction");
             
-            _now = initialInstant;
+            _prevInstant = initialInstant;
             NextNowFunction = prev =>
             {
                 NextNowFunction = nextNowFunction;
@@ -39,23 +51,23 @@ namespace CoreTechs.Common
 
         public DateTimeOffset Now
         {
-            get { return _now = NextNowFunction(_now); }
+            get { return _prevInstant = NextNowFunction(_prevInstant); }
         }
     }
 
-    public sealed class SystemClock : IClock
+
+    /// <summary>
+    /// Clock that returns DateTimeOffset.Now.
+    /// </summary>
+    public  class SystemClock : IClock
     {
         public DateTimeOffset Now { get { return DateTimeOffset.Now; } }
 
-        private SystemClock() { }
+        private static readonly SystemClock Clock = new SystemClock();
 
-        private static readonly SystemClock TheInstance = new SystemClock();
         public static SystemClock Instance
         {
-            get
-            {
-                return TheInstance;
-            }
+            get { return Clock; }
         }
     }
 }
