62bf74f0de154a3ab88a215bc07ca1a0d4070578
Ronnie Overby <roverby@coretechs.net>
2014-08-12 12:05:51 -0400

    testing path traversal methods

---
+++ Tests/FileSystemTests.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using CoreTechs.Common;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class FileSystemTests
+    {
+        [Test]
+        public void EnumerateDownFromRoot()
+        {
+            var file = new FileInfo(@"c:\this\that\the other.dat");
+            
+            var it = file.EnumeratePathDownFromRoot().GetEnumerator();
+            
+            it.MoveNext();
+            var d = (DirectoryInfo) it.Current;
+            Assert.AreEqual(d.FullName, @"c:\");
+
+            it.MoveNext();
+            d = (DirectoryInfo) it.Current;
+            Assert.AreEqual(d.FullName, @"c:\this");
+
+            it.MoveNext();
+            d = (DirectoryInfo) it.Current;
+            Assert.AreEqual(d.FullName, @"c:\this\that");
+
+            it.MoveNext();
+            var f = (FileInfo) it.Current;
+            Assert.AreEqual(f.FullName, @"c:\this\that\the other.dat");
+
+
+
+        }
+        
+        [Test]
+        public void EnumerateUpToRoot()
+        {
+            var file = new FileInfo(@"c:\this\that\the other.dat");
+            
+            var it = file.EnumeratePathUpToRoot().GetEnumerator();
+            
+            it.MoveNext();
+            var f = (FileInfo)it.Current;
+            Assert.AreEqual(f.FullName, @"c:\this\that\the other.dat");
+
+            it.MoveNext();
+            var d = (DirectoryInfo) it.Current;
+            Assert.AreEqual(d.FullName, @"c:\this\that");
+
+            it.MoveNext();
+            d = (DirectoryInfo) it.Current;
+            Assert.AreEqual(d.FullName, @"c:\this");
+
+            it.MoveNext();
+            d = (DirectoryInfo) it.Current;
+            Assert.AreEqual(d.FullName, @"c:\");
+
+
+
+        }
+    }
+}
