e361dd79f59fde604bbcf8e35387f3e67b161018
Ronnie Overby <roverby@coretechs.net>
2014-09-12 10:37:36 -0400

    added search functions for seekable streams

--- CoreTechs.Common/StreamExtensions.cs
+++ CoreTechs.Common/StreamExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace CoreTechs.Common
 {
@@ -20,6 +21,45 @@ namespace CoreTechs.Common
             if (bytes == null) throw new ArgumentNullException("bytes");
 
             return new MemoryStream(bytes.ToArray());
+        }
+
+        /// <summary>
+        /// Seeks to the next occurrence of the target string in the stream.
+        /// </summary>
+        /// <returns>The starting position of the string or null if not found.</returns>
+        public static long? SeekTo(this Stream stream, string target, Encoding encoding = null)
+        {
+            encoding = encoding ?? Encoding.Default;
+            var bytes = encoding.GetBytes(target);
+            return SeekTo(stream, bytes);
+        }
+
+        /// <summary>
+        /// Seeks to the next occurrence of the target byte sequence in the stream.
+        /// </summary>
+        /// <returns>The starting position of the sequence or null if not found.</returns>
+        public static long? SeekTo(this Stream stream, byte[] target)
+        {
+            var t = 0;
+            foreach (var b in stream.EnumerateBytes())
+            {
+                if (b == target[t])
+                {
+                    if (target.Length == t + 1)
+                    {
+                        stream.Seek(-target.Length, SeekOrigin.Current);
+                        return stream.Position;
+                    }
+
+                    t++;
+                }
+                else
+                {
+                    t = 0;
+                }
+            }
+
+            return null;
         }
     }
 }
