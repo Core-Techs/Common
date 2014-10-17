293b6244577bd3fcb45a7ddf7c8e400195cf8b6e
Ronnie Overby <roverby@coretechs.net>
2014-10-17 14:38:55 -0400

    fixed broken code
    dont edit code on github!

--- CoreTechs.Common/ConversionExtensions.cs
+++ CoreTechs.Common/ConversionExtensions.cs
@@ -29,25 +29,25 @@ namespace CoreTechs.Common
             var targetConv = TypeDescriptor.GetConverter(targetType);
             TypeConverter sourceConv = obj != null ? TypeDescriptor.GetConverter(obj.GetType()) : null;
 
-            var attempt2 = Attempt.Get(() => targetConv.ConvertFrom(obj));
-            if (attempt2.Succeeded)
+            var attempt1 = Attempt.Get(() => targetConv.ConvertFrom(obj));
+            if (attempt1.Succeeded)
+                return attempt1.Value;
+
+            Attempt<object> attempt2 = null;
+            if (sourceConv != null)
+                attempt2 = Attempt.Get(() => sourceConv.ConvertTo(obj, targetType));
+
+            if (attempt2 != null && attempt2.Succeeded)
                 return attempt2.Value;
 
-            Attempt<object> attempt3 = null;
-            if (sourceConv != null)
-                attempt3 = Attempt.Get(() => sourceConv.ConvertTo(obj, targetType));
-
-            if (attempt3 != null && attempt3.Succeeded)
+            var attempt3 = Attempt.Get(() => Convert.ChangeType(obj, targetType));
+            if (attempt3.Succeeded)
                 return attempt3.Value;
-
-            var attempt4 = Attempt.Get(() => Convert.ChangeType(obj, targetType));
-            if (attempt4.Succeeded)
-                return attempt4.Value;
 
             if (targetType == typeof(string) && obj != null)
                 return obj.ToString();
 
-            var exceptions = new[] {attempt1, attempt2, attempt3, attempt4}
+            var exceptions = new[] { attempt1, attempt2, attempt3}
                 .Where(x => x != null)
                 .Select(x => x.Exception).ToArray();
 
